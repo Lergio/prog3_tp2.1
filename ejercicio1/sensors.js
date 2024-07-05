e73b462bd920df9fac3f858ea5976fac06f5b6db
Lergio <56664131+Lergio@users.noreply.github.com>
2024-07-04 21:27:39 -0300

    implementacion de sensors.js _ 240704

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,90 +1,29 @@
-class Sensor {}
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+            rel="icon"
+            href="https://academiacimneiber.com/assets/logoClaro-Z6OnHFdL.png"
+        />
+        <title>Monitor de Sensores</title>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css"
+        />
+    </head>
+    <body>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Monitor de Sensores</h1>
+                <div
+                    id="sensor-container"
+                    class="columns is-multiline mt-4"
+                ></div>
+            </div>
+        </section>
 
-class SensorManager {
-    constructor() {
-        this.sensors = [];
-    }
-
-    addSensor(sensor) {
-        this.sensors.push(sensor);
-    }
-
-    updateSensor(id) {
-        const sensor = this.sensors.find((sensor) => sensor.id === id);
-        if (sensor) {
-            let newValue;
-            switch (sensor.type) {
-                case "temperatura": // Rango de -30 a 50 grados Celsius
-                    newValue = (Math.random() * 80 - 30).toFixed(2);
-                    break;
-                case "humedad": // Rango de 0 a 100%
-                    newValue = (Math.random() * 100).toFixed(2);
-                    break;
-                case "presion": // Rango de 960 a 1040 hPa (hectopascales o milibares)
-                    newValue = (Math.random() * 80 + 960).toFixed(2);
-                    break;
-                default: // Valor por defecto si el tipo es desconocido
-                    newValue = (Math.random() * 100).toFixed(2);
-            }
-            sensor.updateValue = newValue;
-            this.render();
-        } else {
-            console.error(`Sensor ID ${id} no encontrado`);
-        }
-    }
-
-    async loadSensors(url) {}
-
-    render() {
-        const container = document.getElementById("sensor-container");
-        container.innerHTML = "";
-        this.sensors.forEach((sensor) => {
-            const sensorCard = document.createElement("div");
-            sensorCard.className = "column is-one-third";
-            sensorCard.innerHTML = `
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            Sensor ID: ${sensor.id}
-                        </p>
-                    </header>
-                    <div class="card-content">
-                        <div class="content">
-                            <p>
-                                <strong>Tipo:</strong> ${sensor.type}
-                            </p>
-                            <p>
-                               <strong>Valor:</strong> 
-                               ${sensor.value} ${sensor.unit}
-                            </p>
-                        </div>
-                        <time datetime="${sensor.updated_at}">
-                            Última actualización: ${new Date(
-                                sensor.updated_at
-                            ).toLocaleString()}
-                        </time>
-                    </div>
-                    <footer class="card-footer">
-                        <a href="#" class="card-footer-item update-button" data-id="${
-                            sensor.id
-                        }">Actualizar</a>
-                    </footer>
-                </div>
-            `;
-            container.appendChild(sensorCard);
-        });
-
-        const updateButtons = document.querySelectorAll(".update-button");
-        updateButtons.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                event.preventDefault();
-                const sensorId = parseInt(button.getAttribute("data-id"));
-                this.updateSensor(sensorId);
-            });
-        });
-    }
-}
-
-const monitor = new SensorManager();
-
-monitor.loadSensors("sensors.json");
+        <script src="sensors.js"></script>
+    </body>
+</html>
